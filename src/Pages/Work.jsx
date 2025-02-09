25b143c0fc15561f76dfa2e7960d226e50dd968d
mohammadashrafulislam1 <ixdell41@gmail.com>
2025-02-09 14:09:26 -0800

    added _

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -58,8 +58,8 @@ const Work = () => {
   useEffect(() => {
     const fetchWorkData = async () => {
         try {
-const realUrl = title?.replace(/_/g, " ");
-            const response = await axios.get(`${endPoint}/works/${workToEdit?.realUrl}`);
+const realUrl = workToEdit.?title?.replace(/_/g, " ");
+            const response = await axios.get(`${endPoint}/works/${realUrl}`);
             setWorkToUpdate(response?.data?.work);
             setFormData(response?.data?.work);
             console.log(response?.data?.work)
